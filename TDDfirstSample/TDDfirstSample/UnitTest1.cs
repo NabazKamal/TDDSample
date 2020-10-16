a79e6089c31a4b25a1221005ef4f1095a5ab6ce1
Nabaz Kamal <Nabaz.Kamal@apoteket.se>
2020-10-16 13:18:29 +0200

    Add new test cases for PriceCalc

--- TDDfirstSample/TDDfirstSample/UnitTest1.cs
+++ TDDfirstSample/TDDfirstSample/UnitTest1.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Dynamic;
 using Xunit;
 
 namespace TDDfirstSample
 {
     //En priscalc som räknar ut kundpris för en varukorg
+
+    //Systemundertest_scenario_expectedbehavior()
     public class UnitTest1
     {
         //we have a basket with items in it
@@ -13,9 +16,38 @@ namespace TDDfirstSample
         //two of same items give 15% discount
         //empty basket will cost 0kr
         [Fact]
-        public void Test1()
+        public void CalculatePrice_EmptyBasket_ReturnsDefault()
         {
+            PriceCalculator pc = new PriceCalculator();
+            decimal price = pc.CalculatePrice(new Basket());
+            Assert.Equal(0, price);
 
+        }
+        [Fact]
+        public void CalculatePrice_NullBasket_ReturnsException()
+        {
+            PriceCalculator pc = new PriceCalculator();
+            //decimal price = pc.CalculatePrice(null);
+            var res = Assert.Throws<ArgumentNullException>(()=> pc.CalculatePrice(null));
+            Assert.Equal("Value cannot be null.", res.Message);
+
+        }
+
+        private class PriceCalculator
+        {
+            internal decimal CalculatePrice(Basket basket)
+            {
+                if (basket == null)
+                    throw new ArgumentNullException();
+                return decimal.Zero;
+            }
+        }
+
+        private class Basket
+        {
+            public Basket()
+            {
+            }
         }
     }
 }

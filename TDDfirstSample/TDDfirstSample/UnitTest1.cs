33d073f23cae162b90373ac7f10871e65a521dc2
Nabaz Kamal <Nabaz.Kamal@apoteket.se>
2020-10-16 09:26:55 +0200

    first commit

---
+++ TDDfirstSample/TDDfirstSample/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace TDDfirstSample
+{
+    //En priscalc som räknar ut kundpris för en varukorg
+    public class UnitTest1
+    {
+        //we have a basket with items in it
+        //items have prices
+        //items can have discounts
+        //three of some items can give 20% discount
+        //two of same items give 15% discount
+        //empty basket will cost 0kr
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
